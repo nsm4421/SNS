e9b017615b8e5a43f21495116d069dd295c11998
N <nsm4421@naver.com>
2025-10-07 17:28:41 +0900

    Fix DI On Supabase Datasource
    supabase codgen라이브러리가 Table객체를 생성할 때 .env파일을 탐색해서 버그가 발생함
    Table객체를 만들기 전에 setClient메써드로 앱 전역에서 사용할 supabase 객체를 초기화시켜줘야 함
    이에 의존성 주입 코드를 수정해줌

--- packages/supabase_datasource/lib/src/dependency_injection/supabase_datasource.module.dart
+++ packages/supabase_datasource/lib/src/dependency_injection/supabase_datasource.module.dart
@@ -2,6 +2,7 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:injectable/injectable.dart';
 import 'package:shared/shared.dart';
 import 'package:supabase/supabase.dart';
+import 'package:supabase_codegen/supabase_codegen.dart';
 import 'package:supabase_datasource/src/auth/local/go_true_async_storage.datasource.dart';
 import 'package:supabase_datasource/src/db/chat/dm/message/dm_message.datasource.dart';
 import 'package:supabase_datasource/src/db/chat/dm/room/dm_room.datasource.dart';
@@ -12,22 +13,28 @@ import 'package:supabase_datasource/supabase_datasource.dart';
 
 @module
 abstract class SupabaseDataSourceModule extends LoggerUtil {
-  final SupabaseClient _client = SupabaseClient(
-    Env.supabaseUrl,
-    Env.supabaseAnonKey,
-    authOptions: AuthClientOptions(
-      authFlowType: AuthFlowType.pkce,
-      pkceAsyncStorage: GoTrueAsyncStorageDataSourceImpl(
-        FlutterSecureStorage(),
-      ),
-      // redirectTo: 'io.your.app://callback', // OAuth 쓸 때만
-      // autoRefreshToken: true, persistSession: true, // 필요 시
-    ),
-  );
+  late final SupabaseClient _client;
 
+  // auth datasource가 가장 먼저 사용됨
+  // auth datasource에 의존성 주입할 때 SupabaseClient를 초기화하고,
+  // supabase codedgen client도 초기화
   @lazySingleton
-  AuthDataSource get auth =>
-      SupabaseAuthDataSourceImpl(_client.auth, logger: logger);
+  AuthDataSource get auth {
+    _client = SupabaseClient(
+      Env.supabaseApiUrl,
+      Env.supabaseAnonKey,
+      authOptions: const AuthClientOptions(
+        authFlowType: AuthFlowType.pkce,
+        pkceAsyncStorage: GoTrueAsyncStorageDataSourceImpl(
+          FlutterSecureStorage(),
+        ),
+        // redirectTo: 'io.your.app://callback', // OAuth 쓸 때만
+        // autoRefreshToken: true, persistSession: true, // 필요 시
+      ),
+    );
+    setClient(_client);
+    return SupabaseAuthDataSourceImpl(_client.auth, logger: logger);
+  }
 
   @lazySingleton
   ProfilesTableDataSource get profileTable =>
--- packages/supabase_datasource/lib/src/env/env.dart
+++ packages/supabase_datasource/lib/src/env/env.dart
@@ -4,8 +4,8 @@ part 'env.g.dart';
 
 @Envied(obfuscate: false)
 abstract class Env {
-  @EnviedField(varName: 'SUPABASE_URL')
-  static const String supabaseUrl = _Env.supabaseUrl;
+  @EnviedField(varName: 'SUPABASE_API_URL')
+  static const String supabaseApiUrl = _Env.supabaseApiUrl;
 
   @EnviedField(varName: 'SUPABASE_ANON_KEY')
   static const String supabaseAnonKey = _Env.supabaseAnonKey;
